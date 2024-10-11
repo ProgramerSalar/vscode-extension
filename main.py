e6b8f61a96191242d78cc6974083f743ead3b80c
ProgramerSalar <manishkumar60708090@gmail.com>
2024-10-12 01:06:59 +0530

    init

---
+++ main.py
@@ -0,0 +1,22 @@
+# def main():
+#     print("hello world")
+    
+# if __name__ == "__main__":
+#     main()
+    
+    
+
+def multiply(a:int,b:int):
+    return a * b
+
+result = multiply(3,5)
+print(result)
+
+result = multiply(result,2)
+result = multiply(result,2)
+result = multiply(result,2)
+result = multiply(result,2)
+result = multiply(result,2)
+
+# result = multiply("three","five")
+print(result)
